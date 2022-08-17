535a19039f1436ccebc52602bfdd086bf984712b
Alexei <alexei.prudnikov@gmail.com>
2022-08-17 22:31:52 +0300

    Add Task02

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    // Task01 - 71. Simplify Path
+    // https://leetcode.com/problems/simplify-path/
     public static String simplifyPath(String path) {
         StringBuilder result = new StringBuilder();
         String folders[] = path.split("/");
@@ -22,11 +26,35 @@ public class Main {
         if (result.length() == 0) result.append("/");
         return result.toString();
     }
+    // Task02 - 20. Valid Parentheses
+    // https://leetcode.com/problems/valid-parentheses/
+    public static boolean isValid(String s) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+        Map<Character, Character> staples = new HashMap<>();
+        staples.put(')', '(');
+        staples.put(']', '[');
+        staples.put('}', '{');
+        for (int i = 0; i < s.length(); i++) {
+            if (staples.containsValue(s.charAt(i))) deque.push(s.charAt(i));
+            else if (staples.containsKey(s.charAt(i)))
+                {
+                    if (deque.peek() == null) return false;
+                    char staple = deque.pop();
+                    if (staple != staples.get(s.charAt(i))) return false;
+                }
+            else return false;
+
+        }
+        if (deque.peek() != null) return false;
+        return true;
+    }
 
     public static void main(String[] args) {
         String path = "/home//foo/";
         path = "/a/./b/../../c/";
-
+        String s = "[({}())[]]";
         System.out.println(simplifyPath(path));
+        System.out.println(isValid(s));
     }
 }
